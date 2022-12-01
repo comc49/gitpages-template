bc715b9274bf27935e884aabb4ca21db49b1ee23
Brian Koo <comc49@gmail.com>
2022-12-01 10:40:58 -0500

    Added react ghpages, chakra

---
+++ react-gitpages-template/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+      Gitpages React Template
+    </div>
+  );
+}
+
+export default App;
